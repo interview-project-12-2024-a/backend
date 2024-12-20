d74d56b649512a8684c740c5e9d4f9eb64e0335d
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2024-12-20 19:15:33 +0000

    feat: add nest config to use env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ChatController } from './controllers/chat/chat.controller';
 import { ChatService } from './services/chat/chat.service';
 import { AuthService } from './services/auth/auth.service';
 import { OpenAiService } from './services/open_ai/open_ai.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, AuthController, ChatController],
   providers: [AppService, ChatService, AuthService, OpenAiService],
 })
