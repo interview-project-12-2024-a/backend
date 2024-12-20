c99920140bc7e4883f9bc3071d7545c131e029ee
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2024-12-20 15:19:42 +0000

    feat: add auth and chat controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { ChatController } from './chat/chat.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AuthController, ChatController],
   providers: [AppService],
 })
 export class AppModule {}
