7a3f1131b5b5398091e9eaadee0dc44e23d79769
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2024-12-22 11:22:20 +0000

    refactor: remove auth feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './controllers/auth/auth.controller';
 import { ChatController } from './controllers/chat/chat.controller';
 import { ChatService } from './services/chat/chat.service';
-import { AuthService } from './services/auth/auth.service';
 import { OpenAiService } from './services/open_ai/open_ai.service';
 import { ConfigModule } from '@nestjs/config';
 import { InterfaceChatService } from './services/interfaces/interface_chat.service';
@@ -14,15 +12,11 @@ import { FirestoreService } from './services/firestore/firestore.service';
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   })],
-  controllers: [AppController, AuthController, ChatController],
+  controllers: [AppController, ChatController],
   providers: [AppService,
   {
     provide: "ChatService",
     useClass: ChatService,
-  },
-  {
-    provide: "AuthService",
-    useClass: AuthService, 
   },
   {
     provide: "GenerativeAIService",
