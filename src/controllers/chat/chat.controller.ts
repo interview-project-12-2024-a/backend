87ae8febc0074190569c5a21789a29a66a32e081
pharaoh583 <75405341+pharaoh583@users.noreply.github.com>
2025-01-06 16:09:02 +0000

    fix: add guard to endpoints

--- src/controllers/chat/chat.controller.ts
+++ src/controllers/chat/chat.controller.ts
@@ -3,13 +3,12 @@ import { Message } from 'src/models/message.model';
 import { InterfaceChatService } from 'src/services/interfaces/interface_chat.service';
 import { FirebaseAuthGuard } from 'src/shared/guards/firebase_auth/firebase_auth.guard';
 
-// TODO: implement guard for this controller
 @Controller('chat')
 export class ChatController {
     constructor(@Inject("ChatService") private chatService: InterfaceChatService) {}
 
     @Get()
-    // @UseGuards(FirebaseAuthGuard)
+    @UseGuards(FirebaseAuthGuard)
     getChat(@Request() req, 
             @Query('lastTimestamp') lastTimestamp?: string): Promise<Array<Message>> {
         // get lasttimestamp param
@@ -17,7 +16,7 @@ export class ChatController {
     }
 
     @Post()
-    // @UseGuards(FirebaseAuthGuard)
+    @UseGuards(FirebaseAuthGuard)
     sendPrompt(@Request() req, @Body() message: Message) {
         return this.chatService.sendPrompt('moises.quispe.arellano@gmail.com', message);
     }
